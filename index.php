7b19decf36db4242afd21809ecf9c6c8f69682ab
godartg <sl.vilcapuma.edwin@gmail.com>
2016-04-13 17:33:36 -0500

    Index prueba
    probando

--- index.php
+++ index.php
@@ -2,10 +2,25 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
-	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<title>Gestor de Proyectos</title>
+	<!-- CSS  -->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
+	<nav class="light-blue lighten-1" role="navigation">
+    	<div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+      		<ul class="right hide-on-med-and-down">
+        		<li><a href="#">Navbar Link</a></li>
+      		</ul>
+
+      		<ul id="nav-mobile" class="side-nav">
+        		<li><a href="#">Navbar Link</a></li>
+      		</ul>
+      		<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    	</div>
+  	</nav>
 	<h1>Sistemas Operativos I</h1>
 	<section id="Opc">
 		<?php include('caja1.php') ?>
