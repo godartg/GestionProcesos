6bbabae8ff0e08acc7f6fe79e4d3d9e08b07780f
godartg <sl.vilcapuma.edwin@gmail.com>
2016-04-13 10:38:34 -0500

    Index.php
    Pagina de Inicio

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<h1>Sistemas Operativos I</h1>
+	<section id="Opc">
+		<div id="Opciones"><a href="Gestion_Procesos.html">FIFO</a></div>
+		<div id="Opciones"><a href="">Round Robin</a></div>
+		<div id="Opciones"><a href="">SJF (Apropiativa)</a></div>
+		<div id="Opciones"><a href="">SJF (No apropiativa)</a></div>
+	</section>
+</body>
+</html>
