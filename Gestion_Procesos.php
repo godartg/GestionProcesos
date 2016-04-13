0677a3a440b4e10803a1c4d1a2e48dde661b6b9e
godartg <sl.vilcapuma.edwin@gmail.com>
2016-04-13 12:12:29 -0500

    Gestion_Proceso
    php

---
+++ Gestion_Procesos.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Gestión de Procesos</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<h1>Gestión de Procesos</h1>
+	<main>
+		<table>
+			<thead>
+				<tr>
+					<td>Proceso</td>
+					<td>Rafaga</td>
+					<td>Tiempo de llegada</td>
+					<td>Orden de llegada</td>
+				</tr>
+			</thead>
+			<tr>
+				<td>Hola</td>
+				<td>kk</td>
+				<td>XD</td>
+				<td>bla</td>
+			</tr>
+			<tr>
+				<td>Hola</td>
+				<td>kk</td>
+				<td>XD</td>
+				<td>bla</td>
+			</tr>
+			<tr>
+				<td>Hola</td>
+				<td>kk</td>
+				<td>XD</td>
+				<td>bla</td>
+			</tr>
+			<tr>
+				<td>Hola</td>
+				<td>kk</td>
+				<td>XD</td>
+				<td>bla</td>
+			</tr>
+		</table>
+		<section>
+			<h3>Diagrama de Gant</h3>
+			<article>
+				<div></div>
+			</article>
+			<article>
+				<div></div>
+			</article>
+			<article>
+				<div></div>
+			</article>
+		</section>
+		<section>
+			T.E: P:1, P:2
+			T.M.E: (P1+P2)/n
+			T.M.E: R
+		</section>
+		<section>
+			<h3></h3>
+			<table>
+				<thead>
+					<tr>
+						<td>Proceso</td>
+						<td>Rafaga</td>
+						<td>Tiempo de llegada</td>
+						<td>Orden de llegada</td>
+					</tr>
+				</thead>
+				<tr>
+					<td>Hola</td>
+					<td>kk</td>
+					<td>XD</td>
+					<td>bla</td>
+				</tr>
+				<tr>
+					<td>Hola</td>
+					<td>kk</td>
+					<td>XD</td>
+					<td>bla</td>
+				</tr>
+				<tr>
+					<td>Hola</td>
+					<td>kk</td>
+					<td>XD</td>
+					<td>bla</td>
+				</tr>
+				<tr>
+					<td>Hola</td>
+					<td>kk</td>
+					<td>XD</td>
+					<td>bla</td>
+				</tr>
+			</table>
+		</section>
+	</main>
+	<footer>
+		
+	</footer>
+</body>
+</html>
