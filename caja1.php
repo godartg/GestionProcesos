a6d8331a8370fa040d4988f9fc7677ecc87cb487
godartg <sl.vilcapuma.edwin@gmail.com>
2016-04-13 15:11:05 -0500

    Index.php
    separando las opciones

---
+++ caja1.php
@@ -0,0 +1,9 @@
+<?php  
+
+echo '	
+	<form action="">
+  		<input type="radio" name="gender" value="male"> Male<br>
+  		<input type="radio" name="gender" value="female"> Female	<br>
+  		<input type="radio" name="gender" value="other"> Other
+	</form>'	
+?>
--- index.php
+++ index.php
@@ -8,10 +8,7 @@
 <body>
 	<h1>Sistemas Operativos I</h1>
 	<section id="Opc">
-		<div id="Opciones"><a href="Gestion_Procesos.html">FIFO</a></div>
-		<div id="Opciones"><a href="">Round Robin</a></div>
-		<div id="Opciones"><a href="">SJF (Apropiativa)</a></div>
-		<div id="Opciones"><a href="">SJF (No apropiativa)</a></div>
+		<?php include('caja1.php') ?>
 	</section>
 </body>
 </html>
