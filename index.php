a540eb44220e095b8480a44d70cba002606aba66
godartg <sl.vilcapuma.edwin@gmail.com>
2016-04-15 11:42:58 -0500

    switch

--- index.php
+++ index.php
@@ -27,24 +27,7 @@
 			<h1 class="header center orange-text">Sistemas Operativos I</h1>
 			<div class="row">
 			    <div class="col s3 grey lighten-5"><p class="z-depth-1"><?php include('caja1.php') ?></p></div>
-			    <div class="col s9 grey lighten-5"><span class="flow-text"><?php switch ($_POST['group1'];) {
-		case "alg1":
-			echo "<h3>FIFO</h3>";
-			break;
-		case "alg2":
-			echo "<h3>SFJ (Apropiativo)</h3>";
-			break;
-		case "alg3":
-			echo "<h3>SFJ (No apropiativo)</h3>";
-			break;
-		case "alg4":
-			echo "<h3>Round Robin</h3>";
-			break;
-		
-		default:
-			echo "<h3>Futuro Algoritmo</h3>";
-			break;
-	} ?></span></div>
+			    <div class="col s9 grey lighten-5"><span class="flow-text"><?php include('caja2.php') ?></span></div>
 			</div>
 
 	    </div>
